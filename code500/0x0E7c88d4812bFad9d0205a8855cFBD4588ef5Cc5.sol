94f6447014f31854a3a9ba8389024e365d333a23
YG <95599912+hellogithub894@users.noreply.github.com>
2023-09-11 01:34:01 +0000

    modified:   address/address_500.txt

---
+++ code500/0x0E7c88d4812bFad9d0205a8855cFBD4588ef5Cc5.sol
@@ -0,0 +1,108 @@
+/**
+ *Submitted for verification at Etherscan.io on 2023-09-11
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+// Telegram: t.me/Pepe911ERC
+// Remembering Patriots Day | Pepe 9/11
+
+// в Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җвў вЎҖвЈҖв Җв Җв Җв Җв ҖвўҖвЎҖвўҖвЈҖвЎҖв Җв ҖвЈҖв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Ҳвў»вЎқвЈҶв Җв Җв Җв Җв ҲвЈ»вЈҝвЈҝв ҹв ӣвЈҝвЈҝвЎҮв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв ҲвўҝвЎҷвЈҰвўҖвЈӨвЈ¶вЈ¶вЈҝвЈ»вЎҹв ҖвЈҖвЈҝвЈҝв ғвЈҖвЎӨв –в ’в ӣв ’в ІвўӨвЎӨвЈ„в Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв ҖвЈ№вЈҫвЈҝвЈҝв ҝв ӣв ӣв ӣв ӣв ӣв ҝвЈҸвЎҪв ӣв Ғв Җв Җв Җв Җв Җв Җв Җв »вЈһвЎҮв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв ҖвўҖвЈ вЈ¶вЎҹв ӣв ӣв ӣв ӣвўүвЎөв Ӣв Җв Җв Җв Җв Җв ҖвЈҖвЎҖв Җв ҷвўҰв ҖвЈҖвЈ в Өв –в ’в ҡв үв үвЈүвЈүвЈҷвЈ¶вЈӨвЈҖв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв ҖвўҖвЈҙвЎҫв ӣв үв ҖвЈ§в Җв Җв ’вў в Ҹв Җв Җв ҖвЈҖвЎҙв ҡв үв үв үв үв үв ҷв ҡвЈҝвЈҚвЎҖв ҖвўҖвЈ вЈҙвЈҫв ҝв ӣв “вўӢвЈүвЈҷвЈҝвЈ·вЎ„в Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв ҖвўҖвЈӨвЎ¶в ӣв Ғв ҖвЈҖвЈҖв ҖвўёвЎҶв Җвў в Ҹв Җв Җвў в һв Ғв Җв Җв ҖвўҖвЈ вЈҙвЈҫвЎҝв ӯв ӯв ҘвўҪвЈ·вЎҝв ӣвўүвЈ в ҙвЈ¶вЈҝвЈҝвЈҝвЈҰвЎҖв ҖвЈ№в Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв ҳвЈҝв Җв Җвў вЎһв Ғв ҲвЈ·вўёвЎҮв ҖвЎҪв Җв Җв Җв Җв Җв Җв ҖвЈ вўһвЎҝв ҡв үв ҖвўҖвЈ вЈӨвЎ–в ҡв Ӣвўів –в үв Ғвў°вЈҝвЈҝвЈҝвЈӯвЈҝв ҝвЈҫв Ҹв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв ҖвўёвЎҮв ҖвўёвЎ„в Җвў вЎҹв Җвў·вЎҖвЎҮв Җв Җв Җв ҖвўҖвЈҙвЈәв өв ӢвўҖвЈ в ҙвЈәвЈҹвЈҝвЎҝвўҝвЈ¶вЈӨвЈјвЎӨв Өв Өв јв ӣв Ӣв үв ҒвўҖвЎјв ғв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв ҳвЈ§в Җв Җв ҷв ’в Ӣв Җв Җв ҲвЈҮв Җв Җв Җв Җв Җв »в ҝв ¶в ¶вЈһвЈүвЎҖв ҖвЈҝвЈҝвЎҝв ҝвўҹвЈҸвЎөв ҹв ҖвЈҖв Җв ҖвўҖвЈҖвЈӨв ҙвўҜвЎҖв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв №вЈҶв Җв Җв ҖвўҖвЎҖв Җв ҖвЈҝв Җв Җв Җв Җв Җв Җв Җв Җв Ҳв “в ’в ӣв ӣв “в “в ӮвўүвЎӯв ҹв Җв Җв Җв үв үв ҷв Ӣв Ғв Җв Җв Җв Ҳв івЎҖв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв Җвў»в Җв ҖвЈҫвЈҝвЎҮв Җв Җвўҝв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв ҖвЈ в –в Ӣв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв ҖвўҖвЈҪвЈ„в Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв ҖвўёвЎҮв ҖвЈ№вЎҪв ¶вЈ„в Җв ёвЎҶв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв ҖвЈҖвЈӨв ¶вўӢвЈҝв ҹв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв Җв ҲвЈ§вЎҫв Ӣв Җв Җвў№вЎ„в ҖвЈ·в Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв ҖвўҖвЈҖвЈҖвЈҖвЈҖвЈҖвЈҖвЈҖвЈӨвЈӨвЈӨв ҙв ¶вўҡвЈӣвЈӯвЈҙв ¶вўҝвЈҝв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв Җвў вЎҹв Ғв Җв Җв ҖвўёвЎҹвЈҰвўҝв Җв Җв Җв Җв Җв Җв ҖвЈ вЎ¶в ҹвўӢвЈүвЈүвЈүвЈүвЈүвЈүвЈҘвЈӨвЈӨвЎҙв ¶в ¶в һв ӣвўӢвЈүвЈЎвЈ¬вЈҝв ҹв Ғв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв ҖвўҝвЈӨв ӨвЈӨвЈҖв ҖвЈҫв ҒвЎҝв ёвЎҮв Җв Җв Җв Җв Җв ҖвЈҹвЈҖв ӣвўӢвЈүвЈ©вЈӯвЈҘвЈӨвЈөвЈӨвЎҙв ҙв ҫв ’в ’в ӣв Ӣв үвўүвЎӨв ҡвЈҝвў·вЎҖв Җв Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җв Җвў вЎҸвЈ¶вЈҜв ¬в ҫвЈ·в ҝв ғвў°вЎҮв Җв Җв Җв Җв °вЈ„в ҷв ӣв үв үв Ғв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв ҖвўҖвЈҙв Ҹв Җв Җв Ҳвў·вЎқвЈ„в Җв Җв Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв Җв Җвў вЎҹвЈ°вўҝвЎҮв Җв Җв Җв Җв ҖвўёвЎҶв Җв ҖвЈҖв Җв Җв Ҳв үв үв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв ҖвўёвЎҖв Җв Җв Җв Җв ҷвў®в івЈ„в Җв Җв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв Җв ҖвўҖвЈҝвЎҪв ғвўёвЎҮв ҖвўҖвЈӨвЈҰв Җв Җвў·в Җв Җв Ҳв івЎ„в Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв Җв ҖвЈҖвЈҫв Үв Җв Җв Җв Җв Җв Ҳвў·вЎқвўҰвЎҖв Җв Җв Җв Җ
+//в Җв Җв Җв Җв Җв ҖвўҖвЈҫвЈұв ғв ҖвўҲвЈ§в Җв ёв ҝв Ӣв Җв Җвўёв Җв Җв җв Җв Җв Җв Җв Җв ҖвўҖвЈҖвЈҖвЈҖвЈҖвЈҖвЈӨвЎӨв Өв ӨвЈӨв ¶в ҡв Ӣв үвЎҮв Җв Җв Җв Җв Җв Җв Җв Җвў№вЎҢвЈҹвўівЈ–вўәв ғ
+//в Җв Җв Җв Җв ҖвўҖвЈҫвўів Ҹв Җв Җв ҳвўҝвЈ„вЈӨвЈҖвЈҖвЈҖвЈҖвЈ»вЈҖвЈҖвЎӨв ҙв ¶в ¶вўҙвЈ–в ӣв үвЈ©в ҹвў»вЈҝвЈҝв Ҳв “вЈҰвўһвЈҒвЈҖвЈ вЈӨвЈӨвЎҮв Җв Җв Җв Җв Җв Җв Җв Җв ёвЎҮвўёвўўвЈҝв үв Җ
+//вЈҫвЎӣв ¶вЎӨв ҙвўҫвЈЈвЎҸв Җв Җв Җв ҖвўҲвЎҮв Җв Җв Җв Җв Җв үвЎҸв Ғв Җв Җв Җв ҖвўҖвЈҲвЈ№вЈҫвЈҘвЈӨвЎҫв ғвў№вЈ·вЈҝвЈҝвЈҝвЈҝвЈҝвЈҝвЈҝвЈҝвЎҮв Җв Җв Җв Җв Җв Җв Җв Җв ҖвЈ§вўҖвўёвЎјвЈҮв Җ
+// в үв ӣвў»вЎҶвў°вЎҸв ҷвЎҮв Җв Җв Җв ҖвўёвЈҝвЈҝвЈ¶вЈ¶вЈ¶вЈ¶вЈ¶вЎ·вў¶вЈ¶вЈҫвЈҝвЈҝвЈҝвЈҝв №вЈҝвЈҝвЈҝвЈ§вЈҖвЈ°вЎҝв ҹв Ӣв Ҳв үв Ғв Җв ҖвЈёвЎҮв Җв Җв Җв Җв Җв Җв Җв Җв ҖвЈҝвўёвўҲвЈ§вўёв Җ
+//в Җв ҖвЈҫвЈҖвЈҢвЈғвЈёв Үв Җв Җв Җв ҖвўёвЈҝвЈҝвЈҝвЈҝвЈҝвЈҝвЈҝвЈ§вЈјвЈҝвЈҝвЈҝвЈӣвЈӢвЈүвЈ„вЈҷвЈЎвЈ вЈЁвЈҪвЈҜвЈҖвЈҖвЈҖвЈҖвЈҖвЈ вЎӨв ҙв ҹв ғв Җв Җв Җв Җв Җв Җв Җв Җв Җв ҷв ҝв һв үв үв Җ
+
+contract Pepe911 {
+    string public name = "Pepe 9/11";
+    string public symbol = "P911";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 initialSupply) {
+        totalSupply = initialSupply * (10 ** uint256(decimals));
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Insufficient balance");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(balanceOf[from] >= value, "Insufficient balance");
+        require(allowance[from][msg.sender] >= value, "Insufficient allowance");
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+}
+
+interface IUniswapV2Router02 {
+    function addLiquidityETH(
+        address token,
+        uint256 amountTokenDesired,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    ) external payable returns (uint256 amountToken, uint256 amountETH, uint256 liquidity);
+}
+
+contract TokenLiquidity is Pepe911 {
+    address private constant UNISWAP_ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    IUniswapV2Router02 public uniswapRouter = IUniswapV2Router02(UNISWAP_ROUTER_ADDRESS);
+
+    constructor(uint256 initialSupply) Pepe911(initialSupply) {}
+
+    function addLiquidity(uint256 tokenAmount, uint256 ethAmount) public payable {
+        require(msg.value == ethAmount, "ETH amount doesn't match msg.value");
+
+        approve(UNISWAP_ROUTER_ADDRESS, tokenAmount);
+
+        uniswapRouter.addLiquidityETH{value: ethAmount}(
+            address(this),
+            tokenAmount,
+            0,
+            0,
+            msg.sender,
+            block.timestamp + 15
+        );
+    }
+}
